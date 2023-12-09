65b64b86814d482ccaea99db90bc9c7b484e4937
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-09 06:33:15 -0500

    Rewrite

--- server.js
+++ server.js
@@ -3,40 +3,37 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-fccTesting(app);
+const bcrypt      = require('bcrypt');
+
+fccTesting(app); //For FCC testing purposes
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-const bcrypt      = require('bcrypt');
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
-    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-        console.log(res);
-    });
-});
+
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
-
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
-    console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-        console.log(res); //true
-    });
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash); 
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); 
+  });
 });
+
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash); 
 
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result); 
 
 //END_SYNC
 
 
-
-
+app.listen(process.env.PORT || 3000, () => {});
 
 
 
