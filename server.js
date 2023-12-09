afa0ec677fc05bf751d0c780cde5c01a7e12c06e
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-09 06:44:17 -0500

    Rewrite

--- server.js
+++ server.js
@@ -14,21 +14,17 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  console.log(hash); 
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-      console.log(res); 
-  });
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res); 
+    });
 });
+  
+
 
 //END_ASYNC
 
 //START_SYNC
-var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-console.log(hash); 
-
-var result = bcrypt.compareSync(myPlaintextPassword, hash);
-console.log(result); 
 
 //END_SYNC
 
